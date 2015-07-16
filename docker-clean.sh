0b125c2fcc0b60a28efe146a085c89b3f0b0ce77
Tianon Gravi <admwiggin@gmail.com>
2015-07-16 09:36:17 -0700

    Improved "docker-clean.sh"

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 set -e
 
-dockerRoot="$(docker -D info | awk -F ': ' '$1 == "Docker Root Dir" { print $2; exit }')"
+dockerInfo="$(docker -D info)"
+dockerRoot="$(echo "$dockerInfo" | awk -F ': ' '$1 == "Docker Root Dir" { print $2; exit }')"
 : ${dockerRoot:=/var/lib/docker}
 
-dockerPs=( $(docker ps -a -q) )
+dockerPs=( $(docker ps -aq) )
 if [ ${#dockerPs[@]} -gt 0 ]; then
 	(
 		set -x
 		df -hT "$dockerRoot"
+		df -hTi "$dockerRoot"
 		docker rm "${dockerPs[@]}" || true
 		df -hT "$dockerRoot"
+		df -hTi "$dockerRoot"
 	)
 fi
 
-dockerUntaggedImages=( $(docker images | awk '/^<none>/ { print $3 }') )
+dockerUntaggedImages=( $(docker images -q --filter 'dangling=true') )
 if [ ${#dockerUntaggedImages[@]} -gt 0 ]; then
 	(
 		set -x
 		df -hT "$dockerRoot"
+		df -hTi "$dockerRoot"
 		docker rmi "${dockerUntaggedImages[@]}" || true
 		df -hT "$dockerRoot"
+		df -hTi "$dockerRoot"
 	)
 fi
