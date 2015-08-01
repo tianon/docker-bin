cd7aae90ee4ffb94160927ccdae3baf6719f7969
Tianon Gravi <admwiggin@gmail.com>
2015-07-31 17:50:16 -0700

    Add "stale status" script

---
+++ docker-stale-status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+IFS=$'\n'
+containers=( $(docker ps -aq --no-trunc) )
+unset IFS
+
+for container in "${containers[@]}"; do
+	name="$(docker inspect -f '{{.Name}}' "$container")"
+	imageId="$(docker inspect -f '{{.Image}}' "$container")"
+	image="$(docker inspect -f '{{.Config.Image}}' "$container")"
+	imageImageId="$(docker inspect -f '{{.Id}}' "$image")"
+	if [ "$imageId" != "$imageImageId" ]; then
+		echo "- $name ($image)"
+	fi
+done
