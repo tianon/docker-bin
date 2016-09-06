772d664fac35a30e4852d67d2ab1f36cd4c6dee6
Tianon Gravi <admwiggin@gmail.com>
2016-09-06 16:45:27 -0700

    Add "--type" to "docker inspect" in "docker-stale-status.sh"

--- docker-stale-status.sh
+++ docker-stale-status.sh
@@ -2,15 +2,15 @@
 set -e
 
 IFS=$'\n'
-containers=( $(docker ps -aq --no-trunc) )
+containers=( $(docker ps --all --quiet --no-trunc) )
 unset IFS
 
 for container in "${containers[@]}"; do
-	name="$(docker inspect -f '{{.Name}}' "$container")"
+	name="$(docker inspect --type container --format '{{.Name}}' "$container")"
 	name="${name#/}"
-	imageId="$(docker inspect -f '{{.Image}}' "$container")"
-	image="$(docker inspect -f '{{.Config.Image}}' "$container")"
-	imageImageId="$(docker inspect -f '{{.Id}}' "$image")"
+	imageId="$(docker inspect --type container --format '{{.Image}}' "$container")"
+	image="$(docker inspect --type container --format '{{.Config.Image}}' "$container")"
+	imageImageId="$(docker inspect --type image --format '{{.Id}}' "$image")"
 	if [ "$imageId" != "$imageImageId" ]; then
 		echo "- $name ($image)"
 	fi
