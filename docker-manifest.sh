539556f9a51aed800c3cc1cad31c3632192f4cfe
Tianon Gravi <admwiggin@gmail.com>
2020-10-20 21:37:17 -0700

    Minor docker-manifest.sh tweaks

--- docker-manifest.sh
+++ docker-manifest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -Eeuo pipefail
 
 # usage: docker-manifest.sh image [image ...]
 #    ie: docker-manifest.sh tianon/speedtest
@@ -12,7 +12,7 @@ declare -A digests=()
 
 get_token() {
 	local repo="$1"; shift
-	if [ -z "${tokens[$repo]}" ]; then
+	if [ -z "${tokens[$repo]:-}" ]; then
 		tokens[$repo]="$(curl -fsSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$repo:pull" | jq --raw-output .token)"
 	fi
 	echo "${tokens[$repo]}"
@@ -36,10 +36,18 @@ get_manifest() {
 	[[ "$repo" == */* ]] || repo="library/$repo"
 	local repoTag="$repo:$tag"
 	# TODO refactor the above to use parse_repotag
-	if [ -z "${manifests[$repoTag]}" ]; then
-		local token="$(get_token "$repo")"
-		local headersFile="$(mktemp docker-manifest.XXXXXXXXXX)"
-		manifests[$repoTag]="$(curl -fsSL -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/$repo/manifests/$tag" -D "$headersFile")"
+	if [ -z "${manifests[$repoTag]:-}" ]; then
+		local token
+		token="$(get_token "$repo")"
+		local headersFile
+		headersFile="$(mktemp -t docker-manifest.XXXXXXXXXX)"
+		local requestHeaders=(
+			-H "Authorization: Bearer $token"
+			-H 'Accept: application/vnd.docker.distribution.manifest.v2+json'
+			-H 'Accept: application/vnd.docker.distribution.manifest.list.v2+json'
+			-H 'Accept: application/vnd.docker.distribution.manifest.v1+json'
+		)
+		manifests[$repoTag]="$(curl -fsSL "${requestHeaders[@]}" "https://registry-1.docker.io/v2/$repo/manifests/$tag" -D "$headersFile")"
 		digests[$repoTag]="$(awk -F ': +' 'tolower($1) == "docker-content-digest" { print $2 }' "$headersFile")"
 		rm "$headersFile"
 	fi
@@ -47,12 +55,14 @@ get_manifest() {
 }
 
 get_digest() {
+	local repoTag
 	repoTag="$(parse_repotag "$@")"
 	get_manifest "$repoTag" > /dev/null
 	echo "${digests[$repoTag]}"
 }
 
 for image in "$@"; do
-	echo >&2 "digest: $(get_digest "$image")"
+	digest="$(get_digest "$image")"
+	echo >&2 "digest: $digest"
 	get_manifest "$image"
 done
